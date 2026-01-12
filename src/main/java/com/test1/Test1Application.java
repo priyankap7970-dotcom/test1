b1941db810fb30cc8bff2074ab9269fb7b52579b
priyanka <priyankap7970@gmail.com>
2026-01-12 20:24:14 +0530

    d

--- src/main/java/com/test1/Test1Application.java
+++ src/main/java/com/test1/Test1Application.java
@@ -5,8 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test1Application {
+<<<<<<< Updated upstream
 ###################ok boss################	
 //some login code with otp
+=======
+	
+########error######################################################
+	//some login code with otp
+>>>>>>> Stashed changes
 	//75%
 	#######this aove lines will be stored in a hidden git area +temp#######
 	ooty
